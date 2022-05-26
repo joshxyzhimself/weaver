5d0d979fb77608b90d1c8f51e2021cc8823031fa
joshxyzhimself <joshxyzhimself@gmail.com>
2022-05-26 19:37:11 +0800

    save

--- index.mjs
+++ index.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 
 import * as telegram from './modules/telegram.mjs';
+import * as uwu from './modules/uwu.mjs';
 import { config } from './modules/config.mjs';
+
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const __production = process.argv.includes('--production');
 
 // goals
 // create record
